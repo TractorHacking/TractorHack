996e3e5a985ce87788f8078a7c671e46ceeecc9b
Jason Iqbal <JasonIqbal@gmail.com>
2021-02-09 23:16:15 -0800

    .

--- javaVersion/app/src/main/java/com/example/trachac/MainActivity.java
+++ javaVersion/app/src/main/java/com/example/trachac/MainActivity.java
@@ -22,14 +22,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+ //       fab.setOnClickListener(new View.OnClickListener() {
+ //           @Override
+ //           public void onClick(View view) {
+ //               Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+ //                       .setAction("Action", null).show();
+ //           }
+ //       });
     }
 
     @Override
