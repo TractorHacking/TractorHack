ff57b4ecc3eac69314d6cfb1c8cd140e7be7a2e4
jdelat09 <jdelat09@calpoly.edu>
2021-03-02 00:42:04 -0800

    created vehicle information file using jasons list

--- reactNativeVersion/tk/js/ConnectedRoot.js
+++ reactNativeVersion/tk/js/ConnectedRoot.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
+<<<<<<< Updated upstream:reactNativeVersion/tk/js/ConnectedRoot.js
 import styles from './Styles.js';
 import Tractor from './TractorImg.js';
 
+=======
+import styles from '../ui/Styles.js';
+import Tractor from '../ui/TractorImg.js';
+import VehicleInfo from './VehicleInformation.js';
+>>>>>>> Stashed changes:reactNativeVersion/tk/components/states/ConnectedRoot.js
 
 class ConnectedRoot extends React.Component {
   constructor(props) {
@@ -14,6 +20,7 @@ class ConnectedRoot extends React.Component {
       <View>
         <Text style = {styles.title}> Connected </Text>
         <Tractor />
+	<VehicleInfo />
         <View style = {styles.padded}>
           <Button title = "Disconnect"
                   onPress = {() => {this.props.changeAppState('splash')}}
