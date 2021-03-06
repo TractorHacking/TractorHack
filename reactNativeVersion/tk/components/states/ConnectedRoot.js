9933050429f5175581e784164b7bee73603b90b1
Jason Iqbal <JasonIqbal@gmail.com>
2021-03-05 23:38:55 -0800

    rename connected to vehicle data in title. arrange files. set react-native-web to working version

--- reactNativeVersion/tk/components/states/ConnectedRoot.js
+++ reactNativeVersion/tk/components/states/ConnectedRoot.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, TextInput, View} from 'react-native';
 import styles from '../ui/Styles.js';
 import Tractor from '../ui/TractorImg.js';
 import VehicleInfo from './VehicleInformation.js';
 import HeaderBar from '../ui/HeaderBar.js';
+import TitleCard from '../ui/TitleCard.js';
+
 
 class ConnectedRoot extends React.Component {
   constructor(props) {
     super(props);
+    this.tractorName = "Tractor Name Here";
+    
+  }
+  
+  titleCard() {
+    
+    return (
+      <View>
+        <TextInput
+          style = {styles.inpTracName}
+          onChangeText = {() => null}
+          defaultView = {this.tractorName}
+          placeholder = "Tractor Name Here"
+          clearButtonMode = "always"
+        />
+        <Tractor />
+      </View>
+    );
   }
   
   render() {
     return (
       <View>
-        <HeaderBar title = "Connected"/>
-        <Tractor />
-	<VehicleInfo />
+        <HeaderBar title = "Vehicle Data"/>
+        <View style = {styles.centerView}>
+          <TitleCard 
+            content = {() => this.titleCard()}
+            backButtonAction = {() => this.props.changeAppState('bluetooth')}
+          />
+          <VehicleInfo />
+        </View>
         <View style = {styles.paddedCenter}>
           <Button title = "Disconnect"
                   onPress = {() => {this.props.changeAppState('splash')}}
