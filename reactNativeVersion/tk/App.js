7fb630964f988bfb30937397cc3dd380a8d77b0d
Takumi Arai <takumiarai6@gmail.com>
2021-03-06 22:29:32 -0800

    Edited HomeCardStatus with render functions

--- reactNativeVersion/tk/App.js
+++ reactNativeVersion/tk/App.js
@@ -9,4 +9,6 @@ export default function App() {
       <StateController />       
     </View>
   );
+
+  <homeCard />
 }
--- reactNativeVersion/tk/components/ui/HomeCardStatus.js
+++ reactNativeVersion/tk/components/ui/HomeCardStatus.js
@@ -1,25 +1,82 @@
 import React from 'react';
 import { Text, View, StyleSheet, Button, Alert, Image } from 'react-native';
 
+
+
 class HomeCardHistory extends React.Component {
-  render() {
-    return(
-      <View style={styles.centerCard}>
-        <Text style={styles.textHeader}>
-          Connection Status
-        </Text>
-        <Image
-          style={styles.image}
-          source={require('../../assets/bluetooth.png')}
-        />
-        <Text style={styles.textBody}>
-          You are not connected to a bluetooth device. Check your bluetooth settings to connect.
-        </Text>
+  state = {
+    connected: false
+  }
+
+  renderHeader = () => {
+    if (this.state.connected === false) {
+      <Text style={styles.textHeader}>
+      Bluetooth Device Not Found
+      </Text>
+    }
+    else {
+      <Text style={styles.textHeader}>
+      Bluetooth Device Paired
+      </Text>
+    }
+  }
+
+  // same image for now
+  renderImage = () => {
+    if (this.state.conneced === false) {
+      <Image
+      style={styles.image}
+      source={require('../../assets/bluetooth.png')}
+      />
+    }
+    else {
+      <Image
+      style={styles.image}
+      source={require('../../assets/bluetooth.png')}
+      />
+    }
+  }
+
+  renderCaption = () => {
+    if (this.state.connected === false) {
+      <Text style={styles.caption}>
+        Check your system's bluetooth settings to pair with the dongle.
+      </Text>
+    }
+    else {
+      <Text style={styles.caption}>
+        Connect to view data and diagnose your vehicle.
+      </Text>
+    }
+
+  }
+
+  renderButton = () => {
+    if (this.state.connected === false) {
+      return(
         <Button
           title="Bluetooth Settings"
           onPress={() => Alert.alert('You pressed a button')}
           color="#1FA2DC"
         />
+      )
+    }
+    else {
+      <Button
+      title="Connect"
+      onPress={() => Alert.alert('You pressed a button')}
+      color="#1FA2DC"
+      />
+    }
+  }
+
+  render() {
+    return(
+      <View style={styles.centerCard}>
+        renderHeader();
+        renderImage();
+        renderCaption();
+        renderButton();
       </View>
     )
   }
@@ -45,7 +102,7 @@ const styles = StyleSheet.create({
     lineHeight: '1.5em',
     textAlign: 'center'
   },
-  textBody: {
+  caption: {
     lineHeight: '1.2em',
     margin: '1em',
     textAlign: 'center'
